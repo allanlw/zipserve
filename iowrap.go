3265209274243649f4ad9cdd65bc134be45f9107
Allan Wirth <allan@allanwirth.com>
2014-11-20 18:38:45 -0600

    Add initial server

---
+++ iowrap.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"io"
+	"os"
+	"sync"
+	"time"
+
+	"golang.org/x/tools/godoc/vfs"
+)
+
+// ReadSeekerAt is a simple wrapper around a ReadSeeker to make it
+// a ReaderAt. Note that using this canibalizes the seeking behavior of
+// the underlying io.ReadSeeker, so it should not be used again
+type ReadSeekerAt struct {
+	wrapped io.ReadSeeker
+	ratLock sync.Mutex
+}
+
+func (f *ReadSeekerAt) ReadAt(p []byte, off int64) (n int, err error) {
+	f.ratLock.Lock()
+	defer f.ratLock.Unlock()
+
+	_, err = f.wrapped.Seek(off, 0)
+	if err != nil {
+		return 0, err
+	}
+
+	n, err = f.wrapped.Read(p)
+	return
+}
+
+// ReadSeekerToReaderAt takes a io.ReadSeeker and returns a new
+// io.ReaderAt that will be a proxy for the ReadSeeker.
+// Note that this new ReaderAt is not as powerful, because it does
+// not allow ReadAt calls to be run in parallel (they lock while waiting)
+func ReadSeekerToReaderAt(rs io.ReadSeeker) io.ReaderAt {
+	return &ReadSeekerAt{wrapped: rs}
+}
+
+// Wraps a vfs.ReadSeekCloser by adding an extra close method
+type wrappedReadSeekCloser struct {
+	wrapped     vfs.ReadSeekCloser
+	after_close io.Closer
+}
+
+func (wrsc *wrappedReadSeekCloser) Close() error {
+	err := wrsc.wrapped.Close()
+	err2 := wrsc.after_close.Close()
+	if err != nil {
+		return err
+	} else {
+		return err2
+	}
+}
+
+func (wrsc *wrappedReadSeekCloser) Read(p []byte) (int, error) {
+	return wrsc.wrapped.Read(p)
+}
+
+func (wrsc *wrappedReadSeekCloser) Seek(offset int64, whence int) (int64, error) {
+	return wrsc.wrapped.Seek(offset, whence)
+}
+
+func WrapReaderWithCloser(rsc vfs.ReadSeekCloser, closer io.Closer) vfs.ReadSeekCloser {
+	return &wrappedReadSeekCloser{rsc, closer}
+}
+
+type FSCloser interface {
+	vfs.FileSystem
+	io.Closer
+}
+
+// wraps a vfs.FileSystem by adding an extra close to call
+type wrappedFileSystem struct {
+	wrapped     FSCloser
+	after_close io.Closer
+}
+
+func (wfs *wrappedFileSystem) Open(p string) (vfs.ReadSeekCloser, error) {
+	return wfs.wrapped.Open(p)
+}
+
+func (wfs *wrappedFileSystem) Lstat(p string) (os.FileInfo, error) {
+	return wfs.wrapped.Lstat(p)
+}
+
+func (wfs *wrappedFileSystem) Stat(p string) (os.FileInfo, error) {
+	return wfs.wrapped.Stat(p)
+}
+
+func (wfs *wrappedFileSystem) ReadDir(p string) ([]os.FileInfo, error) {
+	return wfs.wrapped.ReadDir(p)
+}
+
+func (wfs *wrappedFileSystem) Close() error {
+	err := wfs.wrapped.Close()
+	err2 := wfs.after_close.Close()
+	if err != nil {
+		return err
+	} else {
+		return err2
+	}
+}
+
+func (wfs *wrappedFileSystem) String() string {
+	return wfs.wrapped.String()
+}
+
+func WrapFSCloserWithCloser(fs FSCloser, closer io.Closer) FSCloser {
+	return &wrappedFileSystem{fs, closer}
+}
+
+// FakeDirFileInfo
+type FakeDirFileInfo struct {
+	name string
+}
+
+func (i FakeDirFileInfo) Name() string       { return i.name }
+func (i FakeDirFileInfo) Size() int64        { return 0 }
+func (i FakeDirFileInfo) ModTime() time.Time { return time.Time{} }
+func (i FakeDirFileInfo) Mode() os.FileMode  { return os.ModeDir | 0555 }
+func (i FakeDirFileInfo) IsDir() bool        { return true }
+func (i FakeDirFileInfo) Sys() interface{}   { return nil }
+
+func MakeFakeDirFileInfo(name string) os.FileInfo {
+	return FakeDirFileInfo{name}
+}
---
+++ zipserve.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"archive/zip"
+	"log"
+	"net/http"
+	"os"
+
+	"path"
+	"strings"
+
+	"github.com/allanlw/zipserve/zipfs"
+	"golang.org/x/tools/godoc/vfs"
+	"golang.org/x/tools/godoc/vfs/httpfs"
+)
+
+type ZipOpenFS struct {
+	fs vfs.FileSystem
+}
+
+func NewZipOpeningFS(fs vfs.FileSystem) FSCloser {
+	return &ZipOpenFS{fs}
+}
+
+func (fs *ZipOpenFS) String() string {
+	return "Zip opening fs on (" + fs.fs.String() + ")"
+}
+
+// Returns a ReadSeekCloser (with an emphasis on Close), and an error
+func (fs *ZipOpenFS) recursively_open(prefix string, parts []string) (vfs.ReadSeekCloser, error) {
+	if len(parts) == 0 {
+		return fs.fs.Open(prefix)
+	}
+
+	fi, err := fs.fs.Stat(prefix)
+	if err != nil {
+		return nil, err
+	}
+
+	if fi.IsDir() {
+		return fs.recursively_open(path.Join(prefix, parts[0]), parts[1:])
+	}
+
+	fh, err := fs.Open(prefix)
+	if err != nil {
+		return nil, err
+	}
+
+	zr, err := zip.NewReader(ReadSeekerToReaderAt(fh), fi.Size())
+	if err != nil {
+		fh.Close()
+		return nil, err
+	}
+
+	// Closing the zipfs should also close the backing file
+	zfs := WrapFSCloserWithCloser(zipfs.New(zr, prefix).(FSCloser), fh)
+
+	opened, err := zfs.Open(path.Join("/", path.Join(parts...)))
+	if err != nil {
+		zfs.Close()
+		return nil, err
+	}
+
+	// closing the opened file should also close the zipfs
+	// (which then closes the backing file)
+	return WrapReaderWithCloser(opened, zfs), nil
+}
+
+func (fs *ZipOpenFS) Open(p string) (vfs.ReadSeekCloser, error) {
+	log.Println("open:", p)
+	rsc, err := fs.recursively_open("/", strings.Split(p, "/"))
+	log.Println("open:", err)
+	return rsc, err
+}
+
+func (fs *ZipOpenFS) recursively_stat(prefix string, parts []string, link bool) (os.FileInfo, error) {
+	var fi os.FileInfo
+	var err error
+	if link {
+		fi, err = fs.fs.Lstat(prefix)
+	} else {
+		fi, err = fs.fs.Stat(prefix)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	// successful stat. change stats of zips to folders
+	if len(parts) == 0 {
+		if fs.isOpenableAsZipfs(prefix) {
+			return MakeFakeDirFileInfo(fi.Name()), nil
+		} else {
+			return fi, nil
+		}
+	}
+
+	if fi.IsDir() {
+		return fs.recursively_stat(path.Join(prefix, parts[0]), parts[1:], link)
+	}
+
+	fh, err := fs.fs.Open(prefix)
+	if err != nil {
+		return nil, err
+	}
+	defer fh.Close()
+
+	zr, err := zip.NewReader(ReadSeekerToReaderAt(fh), fi.Size())
+	if err != nil {
+		return nil, err
+	}
+
+	zfs := zipfs.New(zr, prefix).(FSCloser)
+	defer zfs.Close()
+
+	inner := path.Join("/", path.Join(parts...))
+	if link {
+		return zfs.Lstat(inner)
+	} else {
+		return zfs.Stat(inner)
+	}
+}
+
+func (fs *ZipOpenFS) stat(p string, link bool) (os.FileInfo, error) {
+	log.Println("stat:", p)
+	fi, err := fs.recursively_stat("/", strings.Split(p, "/"), link)
+	if err != nil {
+		log.Println("stat:", err)
+	} else {
+		log.Println("stat:", fi.Name(), fi.Size(), fi.Mode(), fi.IsDir(), err)
+	}
+	return fi, err
+}
+
+func (fs *ZipOpenFS) Lstat(p string) (os.FileInfo, error) {
+	return fs.stat(p, true)
+}
+
+func (fs *ZipOpenFS) Stat(p string) (os.FileInfo, error) {
+	return fs.stat(p, false)
+}
+
+func (fs *ZipOpenFS) isOpenableAsZipfs(path string) bool {
+	fi, err := fs.fs.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	fh, err := fs.fs.Open(path)
+	if err != nil {
+		return false
+	}
+	defer fh.Close()
+
+	_, err = zip.NewReader(ReadSeekerToReaderAt(fh), fi.Size())
+	return err == nil
+}
+
+func (fs *ZipOpenFS) recursively_readdir(prefix string, parts []string) ([]os.FileInfo, error) {
+	if len(parts) == 0 && !fs.isOpenableAsZipfs(prefix) {
+		finfos, err := fs.fs.ReadDir(prefix)
+		if err != nil {
+			return finfos, err
+		}
+		for i := 0; i < len(finfos); i++ {
+			if fs.isOpenableAsZipfs(path.Join(prefix, finfos[i].Name())) {
+				finfos[i] = MakeFakeDirFileInfo(finfos[i].Name())
+			}
+		}
+		return finfos, nil
+	}
+
+	fi, err := fs.fs.Stat(prefix)
+	if err != nil {
+		return nil, err
+	}
+
+	if fi.IsDir() {
+		return fs.recursively_readdir(path.Join(prefix, parts[0]), parts[1:])
+	}
+
+	fh, err := fs.fs.Open(prefix)
+	if err != nil {
+		return nil, err
+	}
+	defer fh.Close()
+
+	zr, err := zip.NewReader(ReadSeekerToReaderAt(fh), fi.Size())
+	if err != nil {
+		return nil, err
+	}
+
+	zfs := zipfs.New(zr, prefix).(FSCloser)
+	defer zfs.Close()
+
+	return zfs.ReadDir(path.Join("/", path.Join(parts...)))
+}
+
+func (fs *ZipOpenFS) ReadDir(p string) ([]os.FileInfo, error) {
+	log.Println("readdir:", p)
+	list, err := fs.recursively_readdir("/", strings.Split(p, "/"))
+	log.Println("readdir:", list, err)
+	return list, err
+}
+
+func (fs *ZipOpenFS) Close() error {
+	switch t := fs.fs.(type) {
+	case FSCloser:
+		return t.Close()
+	default:
+		return nil
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Supply one argument, path to serve")
+	}
+	serve := os.Args[1]
+
+	fs := NewZipOpeningFS(vfs.OS(serve))
+	hfs := httpfs.New(fs)
+	fileserver := http.FileServer(hfs)
+	log.Fatal(http.ListenAndServe(":8080", fileserver))
+}
